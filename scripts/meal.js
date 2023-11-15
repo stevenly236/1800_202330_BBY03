8945a17b5e0d4325ef88c42f2c38958988150310
Steven Ly <steven.ly236@gmail.com>
2023-11-14 20:58:08 -0800

    Updated meals.html page to add comments from firestore database, given the meal id. Updated meals.html page based on information of meal id. Fixed comment section boxes. git add .

--- scripts/meal.js
+++ scripts/meal.js
@@ -1,28 +1,23 @@
-function writeComments() {
-    //define a variable for the collection you want to create in Firestore to populate data
-    var commentsRef = db.collection("comments");
+function displaymealInfo() {
+    let params = new URL(window.location.href); //get URL of search bar
+    let ID = params.searchParams.get("docID"); //get value for key "id"
+    console.log( ID );
 
-    commentsRef.add({
-        username: "steepo123",
-        content: "Wow this looks great",
-        last_updated: firebase.firestore.Timestamp.fromDate(new Date("April 23, 2020"))
-    });
-    commentsRef.add({
-        username: "abcd123",
-        content: "I don't like that",
-        last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
-    });
-    commentsRef.add({
-        username: "acasdpojc",
-        content: "I'll make that later!",
-        last_updated: firebase.firestore.Timestamp.fromDate(new Date("January 12, 2023"))
-    });
-    commentsRef.add({
-        username: "adspojc",
-        content: "It was so good!",
-        last_updated: firebase.firestore.Timestamp.fromDate(new Date("January 10, 2023"))
-    })
+    // doublecheck: is your collection called "Reviews" or "reviews"?
+    db.collection("meals")
+        .doc( ID )
+        .get()
+        .then( doc => {
+             thisMeal = doc.data();
+             mealName = doc.data().name;
+            
+            // only populate title, and image
+            document.getElementById("mealName").innerHTML = mealName;
+            let imgEvent = document.querySelector(".meal-img");
+            imgEvent.src = doc.data().image;
+        } );
 }
+displaymealInfo();
 
 function displayCommentsDynamically(collection) {
     let commentTemplate = document.getElementById("commentTemplate"); 
@@ -45,3 +40,28 @@ function displayCommentsDynamically(collection) {
 }
 
 displayCommentsDynamically("comments"); 
+
+function writeComment() {
+    console.log("inside comments");
+    let usercontent = document.getElementById("usercomment").value;
+
+    console.log(usercontent);
+
+    var user = firebase.auth().currentUser;
+    if (user) {
+        var currentUser = db.collection("users").doc(user.uid);
+        var userID = user.uid;
+
+        // Get the document for the current user.
+        db.collection("comments").add({
+            content: usercontent,
+            username: user.displayName,
+            last_updated: firebase.firestore.FieldValue.serverTimestamp()
+        }).then(() => {
+            document.getElementById("usercomment").value = ''
+        });
+    } else {
+        console.log("No user is signed in");
+        window.location.href = 'index.html';
+    }
+}
