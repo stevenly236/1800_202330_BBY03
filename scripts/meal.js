4b687042c5e147081cb01982f85597900d798de6
Steven Ly <steven.ly236@gmail.com>
2023-11-22 12:08:33 -0800

    Fixed comments and rating to href back to the right ID page

--- scripts/meal.js
+++ scripts/meal.js
@@ -6,7 +6,6 @@ function writeComment() {
     let params = new URL(window.location.href); //get URL of search bar
     let ID = params.searchParams.get("docID"); //get value for key "id"
 
-
     console.log(usercontent);
 
     var user = firebase.auth().currentUser;
@@ -20,6 +19,8 @@ function writeComment() {
             userID: userID,
             content: usercontent,
             username: user.displayName,
+            timestamp: firebase.firestore.FieldValue
+            .serverTimestamp() 
         }).then(() => {
             window.location.href = "meal.html?docID=" + ID;
         });
@@ -67,32 +68,7 @@ function displaymealInfo() {
                         starRating += '<span class="material-icons">star_outline</span>';
                     }
                     document.querySelector(".star-rating").innerHTML = starRating;
-                   function displaymealInfo() {
-    let params = new URL(window.location.href); //get URL of search bar
-    let ID = params.searchParams.get("docID"); //get value for key "id"
-    console.log(ID);
 
-    // doublecheck: is your collection called "Reviews" or "reviews"?
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            currentUser = db.collection("users").doc(user.uid); //global
-            console.log(currentUser);
-            db.collection("meals")
-                .doc(ID)
-                .get()
-                .then(doc => {
-                    thisMeal = doc.data();
-                    mealName = doc.data().name;
-
-                    // only populate title, and image
-                    document.getElementById("mealName").innerHTML = mealName;
-                    let imgEvent = document.querySelector(".meal-img");
-                    imgEvent.src = doc.data().image;
-                    //assigning unique ID to the bookmark icon 
-                    //attatching an onclick. calling callback function (with hike's ID)
-                    document.querySelector('i').id = 'save-' + ID;   //guaranteed to be unique
-                    document.querySelector('i').onclick = () => saveBookmark(ID);
-                    document.querySelector('a').href = "profile.html?docID=" + doc.id;
                 
 
                     currentUser.get().then(userDoc => {
@@ -111,20 +87,6 @@ function displaymealInfo() {
 displaymealInfo();
                 
 
-                    currentUser.get().then(userDoc => {
-                        //get the user name
-                        var bookmarks = userDoc.data().bookmarks;
-                        if (bookmarks.includes(ID)) {
-                            document.getElementById('save-' + ID).innerText = 'bookmark';
-                        }
-                    })
-                });
-        } else {
-            console.log("No user is signed in");
-        }
-    });
-}
-displaymealInfo();
 
 
 
@@ -136,6 +98,7 @@ function displayCommentsDynamically(collection) {
 
     db.collection(collection)
         .where("docID", "==", ID)
+     //   .orderBy('timestamp', 'desc')
         .get()
         .then(allComments => {
             allComments.forEach(doc => {
@@ -228,7 +191,10 @@ function writeRating() {
 
         db.collection("meals").doc(ID).update({
             rating: mealRating
-        })
+        }).then(() => {
+            window.location.href = "meal.html?docID=" + ID;
+        });
+        
     } else {
         console.log("No user is signed in");
     }
