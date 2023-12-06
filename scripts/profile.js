a1e2792c4984a9ef6dfa2801affb8c004a462fa9
Henry Tan <henrytann00@gmail.com>
2023-12-05 18:38:56 -0800

    Search page and functinality added, follow button dynamically updated depending on stauts, allowedfollow from search page to be updated onto firebase

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,24 +1,18 @@
-function changeButtonText() {
-  let followButton = document.querySelector('.follow-button');
-
+function changeButtonText(followButton, userID) {
   if (followButton.innerHTML === 'Follow') {
     followButton.innerHTML = 'Following';
-    // Call a function to save the followed user's ID into Firestore
-    let params = new URL(window.location.href)
-    let ID = params.searchParams.get("docID");
-    saveFollowedUserID(ID);
+    saveFollowedUserID(userID);
   } else {
     followButton.innerHTML = 'Follow';
-    // Call a function to remove the followed user's ID from Firestore
-    let params = new URL(window.location.href)
-    let ID = params.searchParams.get("docID");
-    removeFollowedUserID(ID);
+    removeFollowedUserID(userID);
   }
 }
 
 function saveFollowedUserID (userID) {
   firebase.auth().onAuthStateChanged(user => {
     if (user) {
+
+      
 
       db.collection("users").doc(user.uid).update({
         followerID: firebase.firestore.FieldValue.arrayUnion(userID)
@@ -101,59 +95,77 @@ function insertBiographyFirestore(currentUser) {
   })
 }
 
+
 function displayCardsDynamically(collection) {
   let cardTemplate = document.getElementById("mealTemplate"); 
-  
-  
+
   db.collection(collection)
-  .orderBy('last_updated', 'desc')
-  .get()  
-  .then(allMeals=> {
+    .orderBy('last_updated', 'desc')
+    .get()  
+    .then(allMeals => {
       firebase.auth().onAuthStateChanged(user => {
-          if (user) { 
-            let params = new URL(window.location.href);
-            let ID = params.searchParams.get("docID");
+        if (user) { 
+          let params = new URL(window.location.href);
+          let ID = params.searchParams.get("docID");
 
-            db.collection('meals').doc(ID).get().then((mealDoc) => {
-              let currentUser = mealDoc.data().author;
+          db.collection('meals').doc(ID).get().then((mealDoc) => {
+            let currentUser = mealDoc.data().author;
 
-              
-
-
-            
-          
-          allMeals.forEach(doc => { 
+            allMeals.forEach(doc => { 
               if (doc.data().author.includes(currentUser)){
                 db.collection("users").doc(currentUser).get().then((userDoc) =>{
                   insertNameFromFirestore(currentUser);
                   insertBiographyFirestore(currentUser);
                   insertUserNameFromFirestore(currentUser);
-                })
 
-                  var cap = mealDoc.data().description;
-                  var user = mealDoc.data().name;
-                  var imageURL = mealDoc.data().image;
-                  let newcard = mealTemplate.content.cloneNode(true); 
-              console.log(imageURL);
-              console.log(mealDoc.data())
-              newcard.querySelector('.image').src = imageURL;
-              newcard.querySelector('.description').innerHTML = cap;
-              newcard.querySelector('.name').innerHTML = user;
+                  const followButton = document.getElementById('followButton');
+                  const userID = userDoc.id;
 
+                  // Check if the user is already being followed and set the initial button text
+                  isUserFollowed(userID).then((isFollowed) => {
+                    followButton.innerHTML = isFollowed ? 'Following' : 'Follow';
+                  });
 
-              document.getElementById(collection + "-go-here").appendChild(newcard);
-                
+                  // Add event listener to the "Follow" button
+                  followButton.addEventListener('click', function() {
+                    changeButtonText(this, userID); // Pass the button and user ID as arguments
+                  }, { once: true });
+
+                });
               
+                var cap = mealDoc.data().description;
+                var user = mealDoc.data().name;
+                var imageURL = mealDoc.data().image;
+                let newcard = mealTemplate.content.cloneNode(true); 
+                console.log(imageURL);
+                console.log(mealDoc.data())
+                newcard.querySelector('.image').src = imageURL;
+                newcard.querySelector('.description').innerHTML = cap;
+                newcard.querySelector('.name').innerHTML = user;
 
-              
+                document.getElementById(collection + "-go-here").appendChild(newcard);
               }
-          })
-        })
-      }
-  })
-  }) 
+            });
+          });
+        }
+      });
+    }) 
 }
+
 
 displayCardsDynamically("meals");
 
-
+async function isUserFollowed(userID) {
+  return new Promise((resolve, reject) => {
+      firebase.auth().onAuthStateChanged(user => {
+          if (user) {
+              db.collection("users").doc(user.uid).get().then((doc) => {
+                  const followerIDs = doc.data().followerID || [];
+                  resolve(followerIDs.includes(userID));
+              }).catch(reject);
+          } else {
+              resolve(false);
+          }
+      });
+  });
+}
---
+++ scripts/search.js
@@ -0,0 +1,101 @@
+document.getElementById('searchForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    const searchTerm = document.getElementById('searchInput').value;
+
+    searchAndDisplayUsers(searchTerm);
+});
+
+function searchAndDisplayUsers(searchTerm) {
+    const searchResultsContainer = document.getElementById('searchResultsContainer');
+    searchResultsContainer.innerHTML = '';
+
+    db.collection('users')
+        .where('name', '>=', searchTerm.toLowerCase())
+        .where('name', '<=', searchTerm.toLowerCase() + '\uf8ff')
+        .get()
+        .then((querySnapshot) => {
+            const searchTemplate = document.getElementById('searchTemplate');
+
+            querySnapshot.forEach((doc) => {
+                const newUserCard = document.importNode(searchTemplate.content, true);
+                const user = doc.data();
+
+                newUserCard.querySelector('.profile-name').textContent = `${user.name}`;
+                newUserCard.querySelector('.profile-email').textContent = `${user.email}`;
+
+                const followButton = newUserCard.querySelector('.follow-button');
+                followButton.setAttribute('data-user-id', doc.id);
+
+                isUserFollowed(doc.id).then((isFollowed) => {
+                    followButton.innerHTML = isFollowed ? 'Following' : 'Follow';
+                });
+
+                followButton.addEventListener('click', function() {
+                    changeButtonText(this);
+                }, { once: true }); 
+
+                searchResultsContainer.appendChild(newUserCard);
+            });
+
+            if (querySnapshot.size === 0) {
+                const noResultsElement = document.createElement('div');
+                noResultsElement.textContent = 'No users found';
+                searchResultsContainer.appendChild(noResultsElement);
+            }
+        })
+        .catch((error) => {
+            console.error('Error searching Firestore:', error);
+        });
+}
+
+async function isUserFollowed(userID) {
+    return new Promise((resolve, reject) => {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                db.collection("users").doc(user.uid).get().then((doc) => {
+                    const followerIDs = doc.data().followerID || [];
+                    resolve(followerIDs.includes(userID));
+                }).catch(reject);
+            } else {
+                resolve(false);
+            }
+        });
+    });
+}
+
+
+function changeButtonText(followButton) {
+    if (followButton.innerHTML === 'Follow') {
+        followButton.innerHTML = 'Following';
+        const userID = followButton.getAttribute('data-user-id');
+        saveFollowedUserID(userID);
+    } else {
+        followButton.innerHTML = 'Follow';
+        const userID = followButton.getAttribute('data-user-id');
+        removeFollowedUserID(userID);
+    }
+}
+
+function saveFollowedUserID(userID) {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            db.collection("users").doc(user.uid).update({
+                followerID: firebase.firestore.FieldValue.arrayUnion(userID)
+            }).then(() => {
+                console.log("Follow success");
+            });
+        }
+    });
+}
+
+function removeFollowedUserID(userID) {
+    firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+            db.collection("users").doc(user.uid).update({
+                followerID: firebase.firestore.FieldValue.arrayRemove(userID)
+            }).then(() => {
+                console.log("Unfollow success");
+            });
+        }
+    });
+}
