2c9c0e5b0dc3a202e7e8d2713cb00c538ea5e5a2
Henry Tan <henrytann00@gmail.com>
2023-12-05 18:46:21 -0800

    Bug fixes for following page. Now displays all posts by users that the person is following

--- scripts/following.js
+++ scripts/following.js
@@ -56,37 +56,43 @@ displayCardsDynamically("captions"); // database name
 function diplayFollowPost() {
     firebase.auth().onAuthStateChanged(user => {
         if (user) {
-            let userID = user.uid
+            let userID = user.uid;
             db.collection("users").doc(userID).get().then(userDoc => {
-                let followerArray = userDoc.data().followerID || []
+                let followerArray = userDoc.data().followerID || [];
 
-                followerArray.forEach(mealID => {
-                    console.log(mealID)
-                    db.collection("meals").doc(mealID).get().then(mealDoc => {
-    
-                                let description = mealDoc.data().description
-                                let imageurl = mealDoc.data().image
-                                let mealtime = mealDoc.data().mealTime
-                                let mealtitle = mealDoc.data().mealTitle
-                                let name = mealDoc.data().name
+                followerArray.forEach(followedUserID => {
+                    // Get all meals by the followed user
+                    db.collection("meals")
+                        .where("author", "==", followedUserID)
+                        .get()
+                        .then(querySnapshot => {
+                            querySnapshot.forEach(mealDoc => {
+                                let description = mealDoc.data().description;
+                                let imageurl = mealDoc.data().image;
+                                let mealtime = mealDoc.data().mealTime;
+                                let mealtitle = mealDoc.data().mealTitle;
+                                let name = mealDoc.data().name;
 
                                 let cardTemplate = document.getElementById("mealTemplate");
                                 let newcard = cardTemplate.content.cloneNode(true);
 
-                                newcard.querySelector('.image').src = imageurl
-                                newcard.querySelector('.mealTitle').innerText = mealtitle
-                                newcard.querySelector('.description').innerText = description
-                                newcard.querySelector('.name').innerText = name
-                                newcard.querySelector('.mealType').innerText = mealtime
-                                newcard.querySelector('a').href = "meal.html?docID=" + mealID;
+                                newcard.querySelector('.image').src = imageurl;
+                                newcard.querySelector('.mealTitle').innerText = mealtitle;
+                                newcard.querySelector('.description').innerText = description;
+                                newcard.querySelector('.name').innerText = name;
+                                newcard.querySelector('.mealType').innerText = mealtime;
+                                newcard.querySelector('a').href = "meal.html?docID=" + mealDoc.id;
 
                                 document.getElementById("meals-go-here").appendChild(newcard);
-
-                    })
-
-                })
-            })
+                            });
+                        })
+                        .catch(error => {
+                            console.error("Error getting meals:", error);
+                        });
+                });
+            });
         }
-    })
+    });
 }
+
 diplayFollowPost()
